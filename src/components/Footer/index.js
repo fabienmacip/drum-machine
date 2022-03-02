fda2d558f50754937f397884d3095f8ccad9f504
fabienmacip <fabien.macip@gmail.com>
2022-03-02 15:33:19 +0100

    02 Header et Footer en place

---
+++ src/components/Footer/index.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export default function Footer() {
+  return (
+    <Wrapper>
+      <p>
+        Footer
+      </p>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.footer`
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-top : solid 1px;
+`;
--- src/components/UI/Layout.js
+++ src/components/UI/Layout.js
@@ -6,6 +6,11 @@ import GlobalStyle from "./GlobalStyle";
 import lightTheme from "./themes/light.json";
 import darkTheme from "./themes/dark.json";
 
+import Header from "components/Header";
+import Footer from "components/Footer";
+
+
+
 export default function Layout({children}) {
 
   const [isLight, setIsLight] = useState(true);
@@ -17,12 +22,22 @@ export default function Layout({children}) {
   return <ThemeProvider theme={isLight ? lightTheme : darkTheme}>
           <Wrapper>
             <GlobalStyle />
-            {children}
-            <button onClick={handleToggleTheme}>
-              Switch to {isLight ? "dark" : "light"} theme
-            </button>
+            <Header isLight={isLight} handleToggleTheme={handleToggleTheme}/>
+            <Main>
+              {children}
+            </Main>
+            <Footer />
           </Wrapper>
          </ThemeProvider>
 }
 
 const Wrapper = styled.div``;
+
+const Main = styled.div`
+  min-height: calc(100vh - 192px);
+  width: 96%;
+  max-width: 1240px;
+  margin: auto;
+  margin-top: 32px;
+
+`;
